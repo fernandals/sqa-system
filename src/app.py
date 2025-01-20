c18f18cfb9d796b815fb7f9444fc39977408e22d
Fernanda Lustosa <lustosafk@gmail.com>
2025-01-20 11:19:22 -0300

    final main page

--- src/app.py
+++ src/app.py
@@ -6,20 +6,37 @@ st.set_page_config(
     initial_sidebar_state="collapsed",
 )
 
-st.write("# 🗣️ Teste de Síntese de Fala: Avaliação Subjetiva")
+st.markdown(
+    """
+    <script>
+        window.onload = function() {
+            window.scrollTo(0, 0);
+        };
+    </script>
+    """,
+    unsafe_allow_html=True
+)
+
+st.title("🗣️ Teste de Síntese de Fala: Avaliação Subjetiva")
 
 st.markdown(
     """
     ### 🎧 Sua opinião é essencial!
 
-    Estamos testando modelos de **conversão de texto em fala (TTS)** e sua avaliação subjetiva é fundamental para medir a qualidade das vozes geradas. Ouça os áudios atentamente e forneça sua opinião sincera. Sua contribuição ajudará a melhorar a naturalidade e inteligibilidade dos modelos! 🚀
+    Estamos testando modelos de **conversão de texto em fala (TTS)** e sua avaliação subjetiva é fundamental para medir a qualidade das vozes geradas. Durante o experimento, você participará de **três testes diferentes**:
 
-    Este teste faz parte de uma pesquisa na área de aprendizado profundo em fala na **Universidade Federal do Rio Grande do Norte (UFRN)** e contribuirá para avanços no campo. 🎓
-"""
+    1️⃣ **Qualidade**: Você avaliará a **naturalidade e qualidade** dos áudios sintéticos.  
+    2️⃣ **Similaridade**: Você comparará a voz do áudio sintético com a do locutor original para medir o quão semelhantes elas são.  
+    3️⃣ **Inteligibilidade**: Você ouvirá um áudio sintético e deverá transcrever o que foi dito, ajudando a medir a clareza da fala gerada.  
+
+    Sua opinião sincera é essencial para aperfeiçoarmos a naturalidade e inteligibilidade dos modelos! 🚀  
+
+    Este teste faz parte de uma pesquisa na área de aprendizado profundo em fala na **Universidade Federal do Rio Grande do Norte (UFRN)** e contribuirá para avanços no campo. 🎓  
+    """
 )
 
 # blank header as divider
-st.write("###")
+st.text("")
 
 l, m, r = st.columns(3)
 if m.button("Iniciar Teste ▶️", use_container_width=True):
